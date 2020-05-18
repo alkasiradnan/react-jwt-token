7080802aac0f44b4da4b111b4acb4d53345a2729
aalkasir <aalkasir@banquemisr.com>
2020-05-18 14:33:17 +0530

    root path as login

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
           <div className="container mt-3">
             <Switch>
               {/* <Route exact path={["/", "/home"]} component={Home} /> */}
-              <Route exact path="/login" component={Login} />
+              <Route exact path={["/", "/login"]} component={Login} />
               <Route exact path="/register" component={SignUp} />
               <Route exact path="/profile" component={Profile} />
             </Switch>
